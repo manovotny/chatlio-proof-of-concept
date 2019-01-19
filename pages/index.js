eafe19e49d5d250a3b492c5e581869292d1ceee4
Michael Novotny <manovotny@gmail.com>
2019-01-18 22:31:42 -0600

    Adds simulated logging in and out.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,44 @@
-export default () => (
-  <main>
-      <header>
-          <h1>Chatlio Proof of Concept</h1>
-      </header>
-  </main>
-);
+import cookie from 'js-cookie';
+import cookies from 'next-cookies'
+import {Component} from 'react';
+
+export default class extends Component {
+    static async getInitialProps(ctx) {
+        const {loggedIn} = cookies(ctx);
+
+        return {loggedIn};
+    }
+
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            loggedIn: props.loggedIn
+        };
+    }
+
+    render() {
+        const {loggedIn} = this.state;
+
+        return (
+            <main>
+                <header>
+                    <h1>Chatlio Proof of Concept</h1>
+                    <button onClick={() => {
+                        if (loggedIn) {
+                            cookie.remove('loggedIn');
+                        } else {
+                            cookie.set('loggedIn', true);
+                        }
+
+                        this.setState({
+                            loggedIn: !loggedIn
+                        });
+                    }}>
+                        {loggedIn ? 'Logout' : 'Login'}
+                    </button>
+                </header>
+            </main>
+        )
+    }
+}
